f712fee757e7c8d291f1e74eda119f0bb5435b08
kny2609 <kny2609@daum.net>
2020-06-23 17:20:42 +0900

    Ch04 등록

---
+++ Ch04/4-3.py
@@ -0,0 +1,49 @@
+"""
+    날짜 : 2020-06-23
+    이름 : 김나연
+    내용 : 파일 읽고 쓰기 교재 p171
+"""
+
+# 파일 읽기
+file = open('C:/Users/bigdata/Desktop/test.txt')
+line = file.readline()
+
+print('line : ', line)
+file.close() # 파일 닫기
+
+# 여러줄 파일 읽기
+file = open('C:/Users/bigdata/Desktop/test.txt', 'r')  # r : read 읽기모드
+
+while True:
+    line = file.read()
+    if not line:  # 라인이 없으면
+        break
+
+    print(line)
+
+file.close()
+
+# 파일 쓰기
+sample = open('C:/Users/bigdata/Desktop/sample.txt', 'w')  # w : write 쓰기모드
+sample.write('안녕하세요.\n')
+sample.write('반갑습니다.\n')
+sample.write('감사합니다.\n')
+sample.close()
+
+# 구구단 파일 생성하기
+gugudan = open('C:/Users/bigdata/Desktop/gugudan.txt', 'w')
+for a in range(2, 10):
+    gugudan.write('%d단\n' % a)
+    for b in range(1, 10):
+        gugudan.write('%d x %d = %d\n' % (a, b, a*b))
+
+gugudan.close()
+
+# with문(자동으로 파일을 close실행)을 이용한 파일 입출력
+with open('C:/Users/bigdata/Desktop/star.txt', 'w') as star:
+    for i in range(1, 11):
+        star.write('☆'*i)
+        star.write('\n')
+
+
+
